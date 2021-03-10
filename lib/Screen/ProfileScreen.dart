c0d7c60fd4fddb8ca763b9868095a9dc70d8037d
Jeel Bhatti <jeelbhatti9@gmail.com>
2021-03-10 17:36:09 +0530

    ProfileScreen start

--- lib/Screen/ProfileScreen.dart
+++ lib/Screen/ProfileScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instagram_clone/main.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -7,6 +10,7 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  ValueNotifier _notifier = ValueNotifier(5);
   @override
   Widget build(BuildContext context) {
     double h = MediaQuery.of(context).size.height;
@@ -16,7 +20,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: Column(
         children: [
           Container(
-            padding: EdgeInsets.only(top: 40, left: 20, right: 20, bottom: 30),
+            padding: EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 30),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -44,7 +48,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       child: Container(
                         decoration: BoxDecoration(
                             border: Border.all(color: Colors.white, width: 2),
-                            borderRadius: BorderRadius.circular(10)),
+                            borderRadius: BorderRadius.circular(8)),
                         child: Center(
                           child: Icon(
                             Icons.add_sharp,
@@ -54,12 +58,119 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         ),
                       ),
                     ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      Icons.menu_rounded,
+                      color: Colors.white,
+                      size: 27,
+                    ),
                   ],
                 )
               ],
             ),
           )
         ],
+      ),
+      bottomNavigationBar: ValueListenableBuilder(
+        valueListenable: _notifier,
+        builder: (context, c, v) {
+          return DefaultTabController(
+            length: 5,
+            child: Container(
+              color: Colors.black,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                verticalDirection: VerticalDirection.up,
+                children: [
+                  _notifier.value == 1
+                      ? SvgPicture.asset(
+                          "assets/icon/home.svg",
+                          height: 25,
+                          width: 25,
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MyHomePage()));
+                          },
+                          child: SvgPicture.asset(
+                            "assets/icon/home.svg",
+                            color: Colors.white,
+                            height: 25,
+                            width: 25,
+                          ),
+                        ),
+                  _notifier.value == 2
+                      ? SvgPicture.asset(
+                          "assets/icon/loupe.svg",
+                          color: Colors.yellow,
+                          fit: BoxFit.cover,
+                          height: 25,
+                          width: 25,
+                        )
+                      : SvgPicture.asset(
+                          "assets/icon/loupe.svg",
+                          color: Colors.white,
+                          fit: BoxFit.cover,
+                          height: 25,
+                          width: 25,
+                        ),
+                  _notifier.value == 3
+                      ? SvgPicture.asset(
+                          "assets/icon/instagram-reels.svg",
+                          height: 25,
+                          width: 25,
+                          fit: BoxFit.cover,
+                          color: Colors.pinkAccent.shade100,
+                        )
+                      : SvgPicture.asset(
+                          "assets/icon/instagram-reels.svg",
+                          height: 25,
+                          width: 25,
+                          color: Colors.white,
+                        ),
+                  _notifier.value == 4
+                      ? Icon(
+                          FontAwesomeIcons.solidHeart,
+                          color: Colors.redAccent,
+                        )
+                      : Icon(
+                          FontAwesomeIcons.heart,
+                          color: Colors.white,
+                        ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProfileScreen()));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(1),
+                      decoration: BoxDecoration(
+                          color: Colors.white, shape: BoxShape.circle),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.network(
+                          "https://wallpapercave.com/wp/wp6346889.png",
+                          width: 25,
+                          height: 25,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:instagram_clone/Chats_page.dart';
-import 'package:instagram_clone/Screen/ProfileScreen.dart';
 import 'package:instagram_clone/camera.dart';
 
+import 'Screen/ProfileScreen.dart';
 import 'home_page.dart';
 
 void main() => runApp(MyApp());
@@ -38,111 +37,94 @@ class _MyHomePageState extends State<MyHomePage> {
       child: SafeArea(
         bottom: false,
         child: Scaffold(
-            backgroundColor: Colors.black,
-            body: TabBarView(
-              physics: AlwaysScrollableScrollPhysics(),
-              children: <Widget>[
-                camerascreen(cameras),
-                Homepage(),
-                ChatsPage(),
+          backgroundColor: Colors.black,
+          body: TabBarView(
+            physics: AlwaysScrollableScrollPhysics(),
+            children: <Widget>[
+              camerascreen(cameras),
+              Homepage(),
+              ChatsPage(),
+            ],
+          ),
+          bottomNavigationBar: Container(
+            color: Colors.black,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              verticalDirection: VerticalDirection.up,
+              children: [
+                _notifier.value == 1
+                    ? SvgPicture.asset(
+                        "assets/icon/home.svg",
+                        height: 25,
+                        width: 25,
+                      )
+                    : SvgPicture.asset(
+                        "assets/icon/home.svg",
+                        color: Colors.white,
+                        height: 25,
+                        width: 25,
+                      ),
+                _notifier.value == 2
+                    ? SvgPicture.asset(
+                        "assets/icon/loupe.svg",
+                        color: Colors.yellow,
+                        fit: BoxFit.cover,
+                        height: 25,
+                        width: 25,
+                      )
+                    : SvgPicture.asset(
+                        "assets/icon/loupe.svg",
+                        color: Colors.white,
+                        fit: BoxFit.cover,
+                        height: 25,
+                        width: 25,
+                      ),
+                _notifier.value == 3
+                    ? SvgPicture.asset(
+                        "assets/icon/instagram-reels.svg",
+                        height: 25,
+                        width: 25,
+                        fit: BoxFit.cover,
+                        color: Colors.pinkAccent.shade100,
+                      )
+                    : SvgPicture.asset(
+                        "assets/icon/instagram-reels.svg",
+                        height: 25,
+                        width: 25,
+                        color: Colors.white,
+                      ),
+                _notifier.value == 4
+                    ? Icon(
+                        FontAwesomeIcons.solidHeart,
+                        color: Colors.redAccent,
+                      )
+                    : Icon(
+                        FontAwesomeIcons.heart,
+                        color: Colors.white,
+                      ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileScreen()));
+                  },
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      "https://wallpapercave.com/wp/wp6346889.png",
+                      width: 25,
+                      height: 25,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
               ],
             ),
-            bottomNavigationBar: BottomAppBar(
-              color: Colors.black,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                height: h * 0.07,
-                child: ValueListenableBuilder(
-                  valueListenable: _notifier,
-                  builder: (context, c, v) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        _notifier.value == 1
-                            ? SvgPicture.asset(
-                                "assets/icon/home.svg",
-                                height: 25,
-                                width: 25,
-                              )
-                            : GestureDetector(
-                                onTap: () => _notifier.value = 1,
-                                child: SvgPicture.asset(
-                                  "assets/icon/home.svg",
-                                  color: Colors.white,
-                                  height: 25,
-                                  width: 25,
-                                ),
-                              ),
-                        _notifier.value == 2
-                            ? SvgPicture.asset(
-                                "assets/icon/loupe.svg",
-                                color: Colors.yellow,
-                                fit: BoxFit.cover,
-                                height: 25,
-                                width: 25,
-                              )
-                            : GestureDetector(
-                                onTap: () => _notifier.value = 2,
-                                child: SvgPicture.asset(
-                                  "assets/icon/loupe.svg",
-                                  color: Colors.white,
-                                  fit: BoxFit.cover,
-                                  height: 25,
-                                  width: 25,
-                                ),
-                              ),
-                        _notifier.value == 3
-                            ? SvgPicture.asset(
-                                "assets/icon/instagram-reels.svg",
-                                height: 25,
-                                width: 25,
-                                fit: BoxFit.cover,
-                                color: Colors.pinkAccent.shade100,
-                              )
-                            : GestureDetector(
-                                onTap: () => _notifier.value = 3,
-                                child: SvgPicture.asset(
-                                  "assets/icon/instagram-reels.svg",
-                                  height: 25,
-                                  width: 25,
-                                  color: Colors.white,
-                                ),
-                              ),
-                        _notifier.value == 4
-                            ? Icon(
-                                FontAwesomeIcons.solidHeart,
-                                color: Colors.redAccent,
-                              )
-                            : GestureDetector(
-                                onTap: () => _notifier.value = 4,
-                                child: Icon(
-                                  FontAwesomeIcons.heart,
-                                  color: Colors.white,
-                                ),
-                              ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ProfileScreen()));
-                          },
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image.network(
-                              "https://wallpapercave.com/wp/wp6346889.png",
-                              width: 40,
-                              height: 40,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-              ),
-            )),
+          ),
+        ),
       ),
     );
   }
